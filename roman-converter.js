c0646a1d43780437b0698cb8c9b10d1a7fa1fba8
Fernando Buono <fbuono@fi.uba.ar>
2021-09-01 00:41:10 -0300

    [ADD] Adding more projects

---
+++ roman-converter.js
@@ -0,0 +1,15 @@
+// Convert the given number into a roman numeral.
+
+// All roman numerals answers should be provided in upper-case.
+
+const convertToRoman = (num) => {
+    let romanNum = "", i;
+    let romans = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
+    for(i in romans) {
+      while(num >= romans[i]) {
+        romanNum += i
+        num -= romans[i]
+      }
+    }
+    return romanNum
+  }
